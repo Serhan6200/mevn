06933b48b4093cab6dbdb2d4d027e13be68b242f
Serhan ÇINAR <serhan6200@gmail.com>
2024-07-04 05:36:46 -0400

    Navbar added

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,9 +1,20 @@
-<template></template>
+<template><NavBar></NavBar></template>
 <script>
+import NavBar from "@/components/NavBar.vue";
 export default {
   name: "App",
+  components: {
+    NavBar,
+  },
   data() {
     return {};
   },
 };
 </script>
+<style>
+@import url("https://fonts.googleapis.com/css2?family=Mulish:ital,wght@0,200..1000;1,200..1000&display=swap");
+html,
+body {
+  font-family: "Mulish", sans-serif;
+}
+</style>
